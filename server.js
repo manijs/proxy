79bcb881827fb95b9f7f6fea8226dff1e551a305
manijs <temptesting007@gmail.com>
2022-11-19 11:45:29 +0530

    add express server

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
-import * as mockttp from "mockttp";
+import express from "express";
+
+const app = express();
 
 const PORT = process.env.PORT || 6789;
-const https = await mockttp.generateCACertificate();
-const server = mockttp.getLocal({
-  https,
-  http2: true,
-  recordTraffic: false,
+
+app.get("/healthz", (req, res) => {
+  res.status(200).send("OK");
 });
 
-server.forAnyRequest().thenPassThrough();
-await server.start(PORT);
+app.get("/fetch", (req, res) => {
+  res.status(200).send("Hello world");
+});
 
-const caFingerprint = mockttp.generateSPKIFingerprint(https.cert);
-console.log(`Server running on port ${server.port}`);
-console.log(`CA cert fingerprint ${caFingerprint}`);
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
+});
+
+// import * as mockttp from "mockttp";
+
+// const PORT = process.env.PORT || 6789;
+// const https = await mockttp.generateCACertificate();
+// const server = mockttp.getLocal({
+//   https,
+//   http2: true,
+//   recordTraffic: false,
+// });
+
+// server.forAnyRequest().thenPassThrough();
+// await server.start(PORT);
+
+// const caFingerprint = mockttp.generateSPKIFingerprint(https.cert);
+// console.log(`Server running on port ${server.port}`);
+// console.log(`CA cert fingerprint ${caFingerprint}`);
